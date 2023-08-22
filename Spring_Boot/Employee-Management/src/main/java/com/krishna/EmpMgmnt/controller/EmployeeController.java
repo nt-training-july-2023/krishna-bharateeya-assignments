d5d3e5fa4f99b2d29cfdf4d414502bcf4c512690
Krishna-Kumar-NucleusTeq <krishna.bharateeya@nucleusteq.com>
2023-08-22 23:29:46 +0530

    created the login and signup api and also create all entity class of capstone project

--- Spring_Boot/Employee-Management/src/main/java/com/krishna/EmpMgmnt/controller/EmployeeController.java
+++ Spring_Boot/Employee-Management/src/main/java/com/krishna/EmpMgmnt/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.krishna.EmpMgmnt.model.Employee;
 import com.krishna.EmpMgmnt.service.EmployeeService;
+
+
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
